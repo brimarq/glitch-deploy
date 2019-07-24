616703f94a73a0a244a77ae998a9d1e1094d6428
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-24 18:05:53 -0500

    Add files from default glitch hello-express app, .gitignore, updated package.json

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const glitchDeployRoute = require('./routes/glitch-deploy');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+glitchDeployRoute(app);
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
