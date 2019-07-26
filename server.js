831b8510be870830be0d35ee4a7782a2b0b30a89
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-25 21:51:25 -0500

    Add listen port option

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/', function(req, res) {
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
