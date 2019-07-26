07edc56c06e1e2d7c1884862ae37ad85ed6ed2a5
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-25 21:53:14 -0500

    Add git pull functionality

--- routes/glitch-deploy.js
+++ routes/glitch-deploy.js
@@ -8,34 +8,41 @@ function createComparisonSignature (body) {
 }
 
 function compareSignatures (signature, comparison_signature) {
-  const source = Buffer.from(signature);
-  const comparison = Buffer.from(comparison_signature);
+  let source, comparison;
+  if (signature.length !== comparison_signature.length) { return false }
+  source = Buffer.from(signature);
+  comparison = Buffer.from(comparison_signature);
   return crypto.timingSafeEqual(source, comparison); // constant time comparison
 }
 
 function verifyGithubPayload (req, res, next) {
   const { headers, body } = req;
-
-  const signature = headers['x-hub-signature'];
+  const signature = headers['x-hub-signature'] || '';
   const comparison_signature = createComparisonSignature(body);
+  const isVerifiedSignature = compareSignatures(signature, comparison_signature);
 
-  if (!compareSignatures(signature, comparison_signature)) {
+  if (!isVerifiedSignature) {
     return res.status(401).send('Mismatched signatures');
   }
 
-  const { action, ...payload } = body;
-  req.event_type = headers['x-github-event']; // one of: https://developer.github.com/v3/activity/events/types/ 
-  req.action = action;
-  req.payload = payload;
+  // const { ref, action, ...payload } = body;
+  // req.event_type = headers['x-github-event']; // one of: https://developer.github.com/v3/activity/events/types/ 
+  // req.action = action;
+  // req.payload = payload;
+  // req.ack_msg = '*** Received GITHUB PUSH event ***';
   next();
 };
 
-function eventHandler (req, res) {
-  console.log("*** RECEIVED GITHUB PUSH EVENT ***")
-  console.log(req.event_type, req.action, req.payload);
-  return res.send('got authentic event data through my new middleware!');
-};
 
+function eventHandler (req, res) {
+  // const { event_type, action, payload, ack_msg } = req;
+  const repoUrl = req.body.repository.git_url;
+  const repoBranch = process.env.PULL_BRANCH || 'master';
+  const gitPull = `git checkout -- ./ && git pull -X theirs ${repoUrl} ${repoBranch} && refresh`;
+  const output = execSync(gitPull).toString();
+  console.log(output);
+  return res.send();
+};
 
 module.exports = function (app) {
   app.post('/glitch-deploy', verifyGithubPayload, eventHandler);
